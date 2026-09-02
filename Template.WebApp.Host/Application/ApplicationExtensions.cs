fee8b400344f64d4d95af2de74ac39bc3e500d80
usausa <machi.pon@gmail.com>
2026-09-02 19:38:27 +0900

    Update

--- Template.WebApp.Host/Application/ApplicationExtensions.cs
+++ Template.WebApp.Host/Application/ApplicationExtensions.cs
@@ -606,7 +606,7 @@ public static class ApplicationExtensions
         {
             0 => null,
             1 => listeners[0],
-            _ => new ChainListener([.. listeners])
+            _ => new ChainListener(listeners.ToArray())
         };
     }
 
